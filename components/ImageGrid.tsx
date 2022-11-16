3db9eaf01ccd25b80675b51d1af73b4f0be78045
stephen <sely1986@gmail.com>
2022-11-16 16:50:36 -0500

    feat: image grid handles empty list

--- components/ImageGrid.tsx
+++ components/ImageGrid.tsx
@@ -1,25 +1,50 @@
 
-import { Grid, Box, ImageList, ImageListItem } from "@mui/material";
-import { Image } from "modules/gallery/domain/Image.entity";
+import { Grid, Box, Container, ImageList, ImageListItem, Typography } from "@mui/material";
+import { ImageDisplay } from "modules/gallery/domain/Image.entity";
+import { PropsWithChildren } from "react";
 interface ImageGridProps {
-  images: Array<Image>;
+  images?: Array<ImageDisplay>;
 }
+
 export default function ImageGrid({ images }: ImageGridProps) {
-  return <Grid container spacing={2} >
-    <Grid item xs={12}>
-      <Box>{images.length} images</Box>
-    </Grid>
-    <Grid item xs={12}>
-      <ImageList variant="quilted">
-        <Images images={images} />
-      </ImageList>
-    </Grid>
+
+  return <Grid container spacing={2} style={{
+    minWidth: "100%",
+    height: "100vh",
+  }}>
+    {images ?
+      <>
+        <Grid item xs={12}>
+          <Box>{images.length} images</Box>
+        </Grid>
+        <Grid item xs={12}>
+          <ImageList variant="quilted">
+            <Images images={images} />
+          </ImageList>
+        </Grid>
+      </>
+      : <EmptyImageGrid />
+    }
+
   </Grid>
 }
 
-const Images = (props) => {
-  return props.images.map((image) => <ImageListItem>
-    <img key={image.id} src={image.src}></img>
-  </ImageListItem >)
+function EmptyImageGrid() {
+  return <Container maxWidth="lg">
+    <Typography>
+      <h3 align="center">There's nothing here</h3>
+    </Typography>
+  </Container>
+}
+interface ImagesProps {
+  images: Array<ImageDisplay>
+}
+const Images = (props: PropsWithChildren<ImagesProps>) => {
+  return <>
+    {props.images.map((image) => <ImageListItem>
+      <img key={image.id} src={image.src}></img>
+    </ImageListItem >)}
+  </>
+
 }
 
